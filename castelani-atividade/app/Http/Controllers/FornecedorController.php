bf128a80b21192be381c63c384b81092fd2c5e21
Castelani <kaue@presconinformatica.com.br>
2022-10-05 15:11:43 -0300

    Criação da controller

--- castelani-atividade/app/Http/Controllers/FornecedorController.php
+++ castelani-atividade/app/Http/Controllers/FornecedorController.php
@@ -8,30 +8,49 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-
+        $fornecedores = Fornecedor::orderBy('entregador');
+        return view('fornecedor.index')
+                ->with(compact('fornecedores'));
     }
 
     public function create(){
-
+        $fornecedor = null;
+        return view('fornecedor.form')
+                ->with(compact('fornecedor'));
     }
 
-    public function store(){
+    public function store(Request $request){
+        $fornecedor = new Fornecedor();
+        $fornecedor->fill($request->all());
+        $fornecedor->save();
 
+        return redirect()->route('fornecedor.index')->with('success','Cadastrado com sucesso');
     }
 
-    public function show(){
-
+    public function show(int $id){
+        $fornecedor = Fornecedor::find($id);
+        return view('fornecedor.show')
+                ->with(compact('fornecedor'));
     }
 
-    public function edit(){
-
+    public function edit(int $id){
+        $fornecedor = Fornecedor::find($id);
+        return view('fornecedor.form')
+                ->with(compact('tipo'));
     }
 
-    public function update(){
+    public function update(Request $request, int $id){
+        $fornecedor = Fornecedor::find($id);
+        $fornecedor->fill($request->all());
+        $fornecedor->save();
 
+        return redirect()->route('fornecedor.index')->with('success','Atualizado com sucesso');
     }
 
-    public function destroy(){
+    public function destroy(int $id){
+        $fornecedor = Fornecedor::find($id);
+        $fornecedor->delete();
 
+        return redirect()->route('fornecedor.index')->with('danger','Excluído com sucesso');
     }
 }
